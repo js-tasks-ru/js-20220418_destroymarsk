47a9c021b9016072330a92efe3b530949547b3e3
Ertome <destroymarsk@gmail.com>
2022-04-29 00:24:16 +0300

    feat: Added solve for 03 module

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,20 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
+  const props = path.split('.');
 
+  return (object) => {
+    let currentObject = object;
+
+    props.forEach((prop) => {
+      if (!currentObject) {
+        currentObject = null;
+        return;
+      }
+
+      currentObject = currentObject[prop];
+    });
+
+    return currentObject || undefined;
+  };
 }
