d7fbfbd7fc7d70b76399f67b887218f58553d7c7
Ertome <45810834+meowcorp@users.noreply.github.com>
2022-05-18 00:43:05 +0300

    feat: Added tooltip component

--- 06-events-practice/2-tooltip/index.js
+++ 06-events-practice/2-tooltip/index.js
@@ -1,6 +1,69 @@
 class Tooltip {
-  initialize () {
+  static #currentInstance = null;
 
+  constructor() {
+    if (Tooltip.#currentInstance) {
+      return Tooltip.#currentInstance;
+    } else {
+      Tooltip.#currentInstance = this;
+    }
+
+  }
+
+  initialize () {
+    document.addEventListener('pointerover', this.onPointerOver.bind(this));
+    document.addEventListener('pointermove', this.onPointerMove.bind(this));
+  }
+
+  onPointerOver(event) {
+    const elementTooltip = event.target.dataset.tooltip;
+    const prevElementTooltip = event.relatedTarget?.dataset?.tooltip;
+
+    if (!prevElementTooltip && !elementTooltip) return;
+
+    if (prevElementTooltip && !elementTooltip) {
+      this.remove();
+      return;
+    }
+
+    this.render(elementTooltip);
+  }
+
+  onPointerMove(event) {
+    if (!this.element) return;
+
+    this.element.style.position = 'absolute';
+    this.element.style.left = `${event.pageX + 10}px`;
+    this.element.style.top = `${event.pageY + 10}px`;
+  }
+
+  render (tooltipText) {
+    if (this.element) {
+      this.element.textContent = tooltipText;
+      return;
+    }
+
+    const tooltip = document.createElement('div');
+    tooltip.innerHTML = `
+      <div class="tooltip">${tooltipText}</div>
+    `;
+
+    this.element = tooltip.firstElementChild;
+    document.body.append(this.element);
+  }
+
+  remove() {
+    if (!this.element) return;
+
+    this.element.remove();
+    this.element = null;
+  }
+
+  destroy() {
+    document.removeEventListener('pointerover', this.onPointerOver.bind(this));
+    document.removeEventListener('pointermove', this.onPointerMove.bind(this));
+
+    this.remove();
   }
 }
 
